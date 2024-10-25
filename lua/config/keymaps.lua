ff75cb7de81d38b3ed769d8836b4d93efadd7de2
mikeshinoda <53910940+desonglll@users.noreply.github.com>
2024-10-25 22:13:55 +0800

    Update window pick using <c-w>p command.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,18 +2,30 @@ local wk = require("which-key")
 
 local telescope = require("telescope.builtin")
 wk.add({ -- Visual 模式下缩进代码
-    { "<c-w>",     [[<C-\><C-n><C-w>]],          desc = "Exit terminal mode",                            mode = "t" },
-    { "<",         "<gv",                        desc = "Indent left",                                   mode = "v" },
-    { ">",         ">gv",                        desc = "Indent right",                                  mode = "v" },
+    { "<c-w>", [[<C-\><C-n><C-w>]],   desc = "Exit terminal mode",                 mode = "t" },
+    { "<",     "<gv",                 desc = "Indent left",                        mode = "v" },
+    { ">",     ">gv",                 desc = "Indent right",                       mode = "v" },
 
     -- 上下移动选中文本
-    { "J",         ":move '>+1<cr>gv-gv",        desc = "Move selected text DOWN in context",            mode = "v" },
-    { "K",         ":move '<-2<cr>gv-gv",        desc = "Move selected text UP in context",              mode = "v" },
+    { "J",     ":move '>+1<cr>gv-gv", desc = "Move selected text DOWN in context", mode = "v" },
+    { "K",     ":move '<-2<cr>gv-gv", desc = "Move selected text UP in context",   mode = "v" },
 
     -- 保存文件
-    { "<C-s>",     ":w<CR>",                     desc = "Save file",                                     mode = "n" },
-    { "<C-s>",     "<C-c>:w<CR>",                desc = "Save file in visual mode",                      mode = "v" },
-    { "<C-s>",     "<C-c>:w<CR>",                desc = "Save file in insert mode",                      mode = "i" },
+    { "<C-s>", ":w<CR>",              desc = "Save file",                          mode = "n" },
+    { "<C-s>", "<C-c>:w<CR>",         desc = "Save file in visual mode",           mode = "v" },
+    { "<C-s>", "<C-c>:w<CR>",         desc = "Save file in insert mode",           mode = "i" },
+    {
+        "<C-w>p",
+        function()
+            local picked_window_id = require('window-picker').pick_window()
+            if picked_window_id then
+                print("Pick: ", picked_window_id)
+                vim.api.nvim_set_current_win(picked_window_id)
+            end
+        end,
+        desc = "Pick window",
+        mode = "n"
+    },
 
     -- Neotree 切换
     { "<leader>e", ":Neotree toggle<cr>",        desc = "Neotree toggle",                                mode = "n" },
@@ -35,7 +47,7 @@ wk.add({ -- Visual 模式下缩进代码
     -- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
     { "<space>f",  group = "File" },
     { "<space>ff", telescope.find_files,         desc = "Telescope find files",                          mode = "n" },
-    { "<space>fr", telescope.oldfiles,           desc = "Lists previously open files",                   mode = "n" },
+    { "<space>r",  telescope.oldfiles,           desc = "Lists previously open files",                   mode = "n" },
     { "<space>fh", telescope.help_tags,          desc = "Telescope help tags",                           mode = "n" },
     { "<space>s",  group = "Search" },
     { "<space>sg", telescope.live_grep,          desc = "Telescope live grep",                           mode = "n" },
--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -1,7 +1,41 @@
 vim.lsp.set_log_level("debug") -- 其他级别可用： "info", "warn", "error"
 
 return {
-    { 'akinsho/toggleterm.nvim', version = "*", config = true },
+    {
+        's1n7ax/nvim-window-picker',
+        name = 'window-picker',
+        event = 'VeryLazy',
+        version = '2.*',
+        config = function()
+            require 'window-picker'.setup({
+                hint = 'floating-big-letter'
+            })
+        end,
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = function()
+            require("toggleterm").setup({
+                open_mapping = [[<c-\>]],
+                -- 打开新终端后自动进入插入模式
+                start_in_insert = true,
+                -- 在当前buffer的下方打开新终端
+                direction = "horizontal",
+                -- 设置 float 的宽度和高度
+                float_opts = {
+                    border = "curved",         -- 设置浮动窗口的边框样式，可以是 "single", "double", "shadow", 或者其他合法的边框样式
+                    width = 100,               -- 设置浮动窗口的宽度
+                    height = 30,               -- 设置浮动窗口的高度
+                    winblend = 3,              -- 设置浮动窗口的透明度，值范围是 0 到 100，0 表示完全透明，100 表示完全不透明
+                    highlights = {
+                        border = "Normal",     -- 设置边框的高亮颜色
+                        background = "Normal", -- 设置浮动窗口的背景颜色
+                    },
+                },
+            })
+        end
+    },
     {
         "folke/flash.nvim",
         event = "VeryLazy",
