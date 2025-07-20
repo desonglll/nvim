6ab3cc9615ee9436a3f7825bf38297ada5826e8f
desonglll <lindesong666@163.com>
2025-07-20 16:28:57 +0800

    update lsp loading configuration.

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -10,21 +10,50 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
+            automatic_enable = false,
             ensure_installed = {},
         },
         dependencies = {
             "mason-org/mason.nvim",
             "neovim/nvim-lspconfig",
         },
-        config = function(_, opts)
-            require("mason-lspconfig").setup(opts)
-        end,
     },
     {
         "neovim/nvim-lspconfig",
         dependencies = {},
+        keys = {
+            {
+                "grh",
+                mode = { "n" },
+                function()
+                    vim.lsp.inlay_hint.enable()
+                end,
+                desc = "toggle lsp inlay hint",
+            },
+        },
         config = function()
             local lspconfig = require("lspconfig")
+            -- vim.lsp.config()
+            local masonlsp = require("mason-lspconfig").get_installed_servers()
+
+            -- Go here for more detail field: vim.lsp.ClientConfig
+            for _, server in ipairs(masonlsp) do
+                lspconfig[server].setup({
+                    capabilities = {
+                        vim.lsp.protocol.make_client_capabilities(),
+                    },
+                    on_attach = function(client, bufnr)
+                        if client.server_capabilities.inlayHintProvider then
+                            print(string.format("bufnt: %s", bufnr))
+                        end
+                    end,
+                    settings = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                })
+            end
 
             lspconfig.lua_ls.setup({
                 settings = {
